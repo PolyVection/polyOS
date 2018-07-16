db376669da019aae33ab6d27e9e6bbc2f59ef960
Philip Voigt <philip.voigt@unumotors.com>
2018-07-16 13:03:58 +0200

    jenkins update

--- sources/meta-polyos/scripts/bitbake-polyos.sh
+++ sources/meta-polyos/scripts/bitbake-polyos.sh
@@ -126,11 +126,14 @@ bitbake $EXTRA_BITBAKE_FLAGS $target 2>&1 | tee bitbake.log
 build_exit_code=${PIPESTATUS[0]}
 echo "bitbake-polyos EXIT CODE = $build_exit_code"
 
+polyos_version=$(grep 'DISTRO_VERSION =' ${build_bundle_dir}/sources/meta-polyos/conf/distro/polyos.conf | awk '{print $3}' | sed s/\"//g)
+mkdir -p "${build_bundle_dir}/release"
+mkdir -p "${build_bundle_dir}/release/${polyos_version}-(#${build_number})"
+
 if [ $build_exit_code -eq 0 ]; then
-    polyos_version=$(grep 'DISTRO_VERSION =' ${build_bundle_dir}/sources/meta-polyos/conf/distro/polyos.conf | awk '{print $3}' | sed s/\"//g)
-    mkdir -p "${build_bundle_dir}/release"
-    mkdir -p "${build_bundle_dir}/release/${polyos_version}-(#${build_number})"
     cp -rf "${build_workspace_dir}/tmp/deploy/images/voltastream/_PolyOS_release/${polyos_version}/." "${build_bundle_dir}/release/${polyos_version}-(#${build_number})"
+else
+    touch "${build_bundle_dir}/release/${polyos_version}-(#${build_number})/BUILD_FAILED"
 fi
 
 #---------------------------------------------------------------------------
